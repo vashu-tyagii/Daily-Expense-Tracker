4a30d9faa9279c5b58ef7f2aa8cab959584836a2
Vashu Tyagi <vashu.tyagi.connect@gmail.com>
2026-09-20 02:53:33 +0000

    Adding Data Preview Structure !

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ st.set_page_config(page_title="Daily Expense Tracker",
                    page_icon="🔐", layout="centered")
 
 st.markdown("<h1 style='text-align: center;'>Daily Expense Tracker</h1>", unsafe_allow_html=True)
+st.divider()
 
 with st.container(border=True):
-    st.subheader('DataFrame Preview')
+    st.subheader('Daily Expense Table')
+    st.write('Here is Your all Expense Data ')
     st.table(database.get_expense())  # type: ignore
